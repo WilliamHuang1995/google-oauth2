1f4d1df3eb821d9e946eac190bbf5dd97ce12ed3
WilliamHuang1995 <shsidforever@gmail.com>
2018-08-24 16:16:28 +0800

    added sign out

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ gapi.load('auth',()=>{
 })
 document.getElementById("google").addEventListener("click", function(){
     gapi.auth2.getAuthInstance().signIn();
+    document.getElementById("signout").style.display = 'block';
+    document.getElementById("google").style.display = 'none';
 });
+document.getElementById("signout").addEventListener("click",function(){
+    gapi.auth2.getAuthInstance().signOut();
+    document.getElementById("signout").style.display = 'none';
+    document.getElementById("google").style.display = 'block';
+})
 
 
